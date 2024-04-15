a53b6b8409f724e029ae036b50ac99c060783d03
Afranio Dantas <afraniodantas0224@gmail.com>
2024-04-14 21:47:56 -0300

    saving study

--- src/classes/A13-implementando-type-alias.ts
+++
@@ -1,7 +0,0 @@
-export abstract class TipoPessoa {
-  protected abstract nome: string;
-  protected abstract sobrenome: string;
-  protected abstract nomeCompleto(): string;
-}
-
-export class Pessoa extends TipoPessoa {}
