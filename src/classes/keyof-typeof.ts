a741e576aac6f722125f109badc7ae3317cd22cf
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-04-29 08:58:48 -0300

    saving progress in keyof and typeof

---
+++ src/classes/keyof-typeof.ts
@@ -0,0 +1,16 @@
+// criando algo para traduzir cores
+type CoresObj = {
+  vermelho: string;
+  verde: string;
+  azul: string;
+};
+
+const coresObj: CoresObj = {
+  vermelho: 'red',
+  verde: 'green',
+  azul: 'blue',
+};
+
+function traduzirCor(cor: string, cores: CoresObj) {
+  return core[cor];
+}
