b5c38ef33ab08b9d4c321a512c64b8e542e1dde8
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-05-29 12:27:28 -0300

    study: generics

---
+++ src/classes/A28-generics-7.ts
@@ -0,0 +1,18 @@
+export function unirObjetos<T, U>(obj1: T, obj2: U): T & U {
+  return Object.assign({}, obj1, obj2);
+}
+
+const comida = {
+  frango: 'S',
+  arroz: 'S',
+  manteiga: 'N',
+  pao: 'N',
+  aveia: 'S',
+};
+
+const limpeza = {
+  frango: 2,
+  shampoo: 2,
+};
+
+console.log(unirObjetos(comida, limpeza));
