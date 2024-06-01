23fcff35e8e0edc183f64384fd2da7ba79416841
Afranio Dantas <afraniodantas0224@gmail.com>
2024-05-31 23:59:15 -0300

    study: namespace imports

--- src/namespaces/index.ts
+++ src/namespaces/index.ts
@@ -1,17 +1,4 @@
-/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="module.ts" />
 
-namespace MeuNameSpace {
-  export class PessoaDoNameSpace {
-    constructor(public nome: string) {}
-  }
-
-  export namespace OutroNameSpace {
-    export const nomeDoNameSpace = 'Lá-ele';
-  }
-}
-
-const pessoaEscopo = new MeuNameSpace.PessoaDoNameSpace('Afranin');
-console.log(pessoaEscopo);
 console.log(MeuNameSpace.OutroNameSpace.nomeDoNameSpace);
-
-export default 1;
