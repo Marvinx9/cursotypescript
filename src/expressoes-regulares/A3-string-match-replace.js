bea149068c678592492d98c72adf3e4d87c5bcb0
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-06-14 14:20:14 -0300

    feat: ensure manipulation of strings

--- src/expressoes-regulares/A3-string-match-replace.js
+++ src/expressoes-regulares/A3-string-match-replace.js
@@ -1,9 +1,17 @@
 //match return an array with value found
-
+/* pode ocorrer casos em que eu tenha que pegar valores dentro de um grupo específico
+(.1.(.2.))(.3.)(.4.)  $1 $2 $3 $4 a sequência se mantém na ordem de leitura */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { text } = require('./A2-text-data');
 const regExp1 = /João|maria/gi;
 console.log(text);
 console.log(text.match(regExp1));
-console.log(text.replace(/João/i, 'Afrânio'));
+// console.log(text.replace(/João|Maria/gi, 'Afrânio'));
+
+// console.log(text.replace(/(João|Maria)/gi, '<b>$1</b>'));
+console.log(
+  text.replace(/(João|Maria)/gi, function (input) {
+    return '##' + input.toUpperCase() + '##';
+  }),
+);
